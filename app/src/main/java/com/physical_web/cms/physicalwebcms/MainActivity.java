623dc9954010bba52ab161efa445b5f5affc602d
Kayali <me@kayali.io>
2017-06-01 13:15:36 -0700

    Initalized Android Studio
    Target is android 18 - because that is the minimum version that has
    support for the methods we need in BluetoothGatt

---
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
@@ -0,0 +1,13 @@
+package com.physical_web.cms.physicalwebcms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
