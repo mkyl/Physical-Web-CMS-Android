676d2f97cbb04fcea2b101da7f8350c129cfa7e8
Kayali <me@kayali.io>
2017-06-12 00:06:53 +0400

    Authentication to Google Drive successful

--- app/src/main/java/com/physical_web/cms/physicalwebcms/DriveSetupActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/DriveSetupActivity.java
@@ -1,13 +1,83 @@
 package com.physical_web.cms.physicalwebcms;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class DriveSetupActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+public class DriveSetupActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient apiClient;
+
+    private static final String TAG = "Physical Web CMS";
+    final int RESOLVE_CONNECTION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive_setup);
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        final Button setupButton = (Button) findViewById(R.id.start_setup_button);
+        setupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // start setup
+                apiClient.connect();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d(TAG, "Drive connection successful");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
+            } catch (IntentSender.SendIntentException e) {
+                // TODO Unable to resolve, message user appropriately
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+                                    final Intent data) {
+        switch (requestCode) {
+            case RESOLVE_CONNECTION_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    apiClient.connect();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
@@ -1,13 +1,29 @@
 package com.physical_web.cms.physicalwebcms;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setupDrive();
+    }
+
+    private void setupDrive() {
+        Boolean driveSetupComplete = sharedPreferences.getBoolean("drive-setup-complete", true);
+        if (!driveSetupComplete) {
+            Intent driveSetupIntent = new Intent(this, DriveSetupActivity.class);
+            startActivity(driveSetupIntent);
+            sharedPreferences.edit().putBoolean("drive-setup-complete", true).apply();
+        }
     }
 }
