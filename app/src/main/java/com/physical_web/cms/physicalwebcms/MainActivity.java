e1f509beb2ae72cd67ee4b9bffba05c7957aeca9
Kayali <me@kayali.io>
2017-06-30 13:46:39 +0400

    Removed tester code

--- app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements EddstoneBeaconEvents {
+public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     BluetoothManager bluetoothManager;
 
@@ -25,8 +25,6 @@ public class MainActivity extends AppCompatActivity implements EddstoneBeaconEve
 
     @Override
     protected void onDestroy() {
-        // close database
-        //beaconDBManager.close();
         super.onDestroy();
     }
 
@@ -38,28 +36,4 @@ public class MainActivity extends AppCompatActivity implements EddstoneBeaconEve
         SetupManager setupManager = new SetupManager(this);
         setupManager.checkRequirements();
     }
-
-    public void checkGatt(View v) {
-        bluetoothManager = new BluetoothManager(this);
-        bluetoothManager.listConfigurableEddystoneBeacons(this);
-    }
-
-    @Override
-    public void onScanComplete(List<BluetoothDevice> beacons) {
-        for(BluetoothDevice device : beacons) {
-            Log.d(TAG, "Connecting to device with address: " + device.getAddress());
-            //bluetoothManager.supportsEddystoneURL(device, this);
-            bluetoothManager.updateBeaconUri(device, "example.com", this);
-        }
-    }
-
-    @Override
-    public void uriWriteCallback (BluetoothDevice device, final int status) {
-        MainActivity.this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ((TextView) findViewById(R.id.helloWorld)).setText("Updated URI status = " + status);
-            }
-        });
-    };
 }
