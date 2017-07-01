f167d6de8c71a426f3115b86213076acd70ec444
Kayali <me@kayali.io>
2017-07-01 14:26:39 +0400

    navigation drawer

--- app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/MainActivity.java
@@ -1,22 +1,36 @@
 package com.physical_web.cms.physicalwebcms;
 
 import android.bluetooth.BluetoothDevice;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    BluetoothManager bluetoothManager;
+
+    private String[] menuTitles;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuTitles = getResources().getStringArray(R.array.navigation_drawer_items);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuTitles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
         // TODO add this to menu
         // Intent entrollmentIntent = new Intent(this, EnrollmentActivity.class);
@@ -36,4 +50,15 @@ public class MainActivity extends AppCompatActivity {
         SetupManager setupManager = new SetupManager(this);
         setupManager.checkRequirements();
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+        private void selectItem(int position) {
+        }
+    }
 }
+
