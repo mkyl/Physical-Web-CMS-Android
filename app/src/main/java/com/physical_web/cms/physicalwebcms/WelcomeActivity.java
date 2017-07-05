336a93dd6ab406c82e1d084bb296a9ea2518e0bb
Kayali <me@kayali.io>
2017-07-05 16:36:02 +0400

    Optimized imports, refactored some methods

--- app/src/main/java/com/physical_web/cms/physicalwebcms/WelcomeActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/WelcomeActivity.java
@@ -8,8 +8,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
