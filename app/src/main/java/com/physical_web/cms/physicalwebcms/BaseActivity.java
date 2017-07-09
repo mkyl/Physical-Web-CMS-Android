f290603118e745ea00f862b76fc1715b12dc0019
Kayali <me@kayali.io>
2017-07-09 23:03:46 +0400

    conflict resolution working both directions

--- app/src/main/java/com/physical_web/cms/physicalwebcms/BaseActivity.java
+++ app/src/main/java/com/physical_web/cms/physicalwebcms/BaseActivity.java
@@ -44,8 +44,8 @@ public class BaseActivity extends AppCompatActivity implements
         setupManager = new SetupManager(this);
         fileManager = new FileManager(this);
 
-        // fileManager.createDemoFile();
-        fileManager.deleteDemoFile();
+        fileManager.createDemoFile();
+        // fileManager.deleteDemoFile();
 
         File folderToSync = fileManager.getRootFolder();
         contentSynchronizer = new ContentSynchronizer(this, folderToSync);
