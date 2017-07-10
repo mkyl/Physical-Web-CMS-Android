7a139e186960f7f198ee4b6b1238f2467c578dab
Kayali <me@kayali.io>
2017-07-10 18:38:45 +0400

    Created exhibits fragment

---
+++ app/src/main/java/layout/ExhibitFragment.java
@@ -0,0 +1,45 @@
+package layout;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.physical_web.cms.physicalwebcms.R;
+
+/**
+ * Fragment for displaying information about exhibits
+ */
+public class ExhibitFragment extends ContentFragment {
+    public ExhibitFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ExhibitFragment.
+     */
+    public static ExhibitFragment newInstance(String param1, String param2) {
+        ExhibitFragment fragment = new ExhibitFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exhibit, container, false);
+    }
+}
