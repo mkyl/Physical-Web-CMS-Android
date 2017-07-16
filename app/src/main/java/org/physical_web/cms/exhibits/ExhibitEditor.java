6981b4a13a478291ecfc6188356e39c6afafbaad
Kayali <me@kayali.io>
2017-07-16 20:31:03 +0300

    Communicate info about exhibit to editor

--- app/src/main/java/org/physical_web/cms/exhibits/ExhibitEditor.java
+++ app/src/main/java/org/physical_web/cms/exhibits/ExhibitEditor.java
@@ -2,6 +2,7 @@ package org.physical_web.cms.exhibits;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import org.physical_web.cms.R;
  * A simple {@link Fragment} subclass.
  */
 public class ExhibitEditor extends Fragment {
+    private final static String TAG = ExhibitEditor.class.getSimpleName();
     public ExhibitEditor() {
         // Required empty public constructor
     }
@@ -19,6 +21,13 @@ public class ExhibitEditor extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle b = getArguments();
+        if (b == null)
+            throw new IllegalArgumentException("No exhibit provided to edit");
+
+        String s = b.getString("exhibit-name");
+        Log.d(TAG, "Editing exhibit with name: " + s);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_exhibit_editor, container, false);
     }
