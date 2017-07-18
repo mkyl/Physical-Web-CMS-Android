8538b162c3fae34467536cc420db12ba209252a4
Kayali <me@kayali.io>
2017-07-18 17:30:15 +0300

    Detection of content type based on MIME signature

--- app/src/main/java/org/physical_web/cms/exhibits/ExhibitContent.java
+++ app/src/main/java/org/physical_web/cms/exhibits/ExhibitContent.java
@@ -1,16 +1,33 @@
 package org.physical_web.cms.exhibits;
 
+import android.webkit.MimeTypeMap;
+
 import java.io.File;
 
 /**
  * Abstract class representing the types of content that can be stored in an exhibit
  */
 public abstract class ExhibitContent {
+    private final static String MIME_TEXT_PREFIX = "text/";
+    private final static String MIME_AUDIO_PREFIX = "audio/";
+    private final static String MIME_IMAGE_PREFIX = "image/";
+    private final static String MIME_VIDEO_PREFIX = "video/";
+
     private String contentName;
 
-    public static ExhibitContent fromRawFile(File contentFile) {
-        // TODO figure out what type of file it is
-        return new TextContent();
+    public static ExhibitContent fromFile(File contentFile) {
+        String mimeType = MimeTypeMap.getFileExtensionFromUrl(contentFile.toURI().toString());
+
+        if (mimeType.startsWith(MIME_TEXT_PREFIX))
+            return new TextContent();
+        else if (mimeType.startsWith(MIME_AUDIO_PREFIX))
+            return new SoundContent();
+        else if (mimeType.startsWith(MIME_IMAGE_PREFIX))
+            return new ImageContent();
+        else if (mimeType.startsWith(MIME_VIDEO_PREFIX))
+            return new VideoContent();
+        else
+            throw new IllegalArgumentException("Exhibit content has unrecognized file type");
     }
 
     public void setContentName(String contentName){
@@ -18,7 +35,6 @@ public abstract class ExhibitContent {
     };
 
     public abstract String toHTML();
-
 }
 
 class TextContent extends ExhibitContent {
