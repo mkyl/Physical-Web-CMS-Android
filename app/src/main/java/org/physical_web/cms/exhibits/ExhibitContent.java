153f3d565ebadf8b3d256f4ffa5f73da628491b5
Kayali <me@kayali.io>
2017-07-24 16:26:24 +0300

    Better content type detection by peeking at magic bits

--- app/src/main/java/org/physical_web/cms/exhibits/ExhibitContent.java
+++ app/src/main/java/org/physical_web/cms/exhibits/ExhibitContent.java
@@ -1,13 +1,22 @@
 package org.physical_web.cms.exhibits;
 
+import android.util.Log;
 import android.webkit.MimeTypeMap;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
 
 /**
  * Abstract class representing the types of content that can be stored in an exhibit
  */
 public abstract class ExhibitContent {
+    private final static String TAG = ExhibitContent.class.getSimpleName();
+
     private final static String MIME_TEXT_PREFIX = "text/";
     private final static String MIME_AUDIO_PREFIX = "audio/";
     private final static String MIME_IMAGE_PREFIX = "image/";
@@ -16,18 +25,27 @@ public abstract class ExhibitContent {
     private String contentName;
 
     public static ExhibitContent fromFile(File contentFile) {
-        String mimeType = MimeTypeMap.getFileExtensionFromUrl(contentFile.toURI().toString());
+        ExhibitContent result;
+        // easy check: look at extension and determine file type
+        String mimeType = mimeTypeFromExtension(contentFile);
+
+        // more resource intensive, peek into content if no file extension
+        if(mimeType == null)
+            mimeType = mimeTypeFromContent(contentFile);
 
         if (mimeType.startsWith(MIME_TEXT_PREFIX))
-            return new TextContent(contentFile);
+            result = new TextContent(contentFile);
         else if (mimeType.startsWith(MIME_AUDIO_PREFIX))
-            return new SoundContent(contentFile);
+            result = new SoundContent(contentFile);
         else if (mimeType.startsWith(MIME_IMAGE_PREFIX))
-            return new ImageContent(contentFile);
+            result = new ImageContent(contentFile);
         else if (mimeType.startsWith(MIME_VIDEO_PREFIX))
-            return new VideoContent(contentFile);
+            result = new VideoContent(contentFile);
         else
             throw new IllegalArgumentException("Exhibit content has unrecognized file type");
+
+        result.setContentName(contentFile.getName());
+        return result;
     }
 
     public void setContentName(String contentName){
@@ -35,6 +53,29 @@ public abstract class ExhibitContent {
     }
 
     public abstract String toHTML();
+
+    private static String mimeTypeFromExtension(File contentFile) {
+        String fileName = contentFile.toURI().toString();
+        String extensionFromUrl = MimeTypeMap.getFileExtensionFromUrl(fileName);
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extensionFromUrl);
+    }
+
+    private static String mimeTypeFromContent(File contentFile) {
+        try {
+            InputStream is = new BufferedInputStream(new FileInputStream(contentFile));
+            String mimeType = URLConnection.guessContentTypeFromStream(is);
+            is.close();
+
+            if (mimeType == null)
+                throw new UnsupportedEncodingException();
+
+            return mimeType;
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to determine mime type from content: " + e);
+            // "application/octet-stream" AKA it's binary data but I can't tell you much more
+            return "application/octet-stream";
+        }
+    }
 }
 
 class TextContent extends ExhibitContent {
